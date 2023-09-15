868b00ace96c2d4b72a11df0e618f266e305cfe6
Gerendás Zoltán <gerendas.zoltan@wmtech.hu>
2023-09-15 09:40:46 +0200

    elso

---
+++ elso.py
@@ -0,0 +1,40 @@
+'''
+print(34+5)
+print("Szia"+" "+"!")
+print("Szia"+" "+"!"*3)     # szöveg szorozható
+#print("Szia"+9+"!")         Tipus eltérés
+print("Szia",end=" ")
+print("barátom!")
+print()
+print("Szia",end="-"*5)
+print("barátom!")
+print("1234\n\n567\n89")
+print()
+print("1234\b567")  # \b back sapce
+print("1234\"33567")
+print('1234\'33567')
+print('1234\\33567')
+print("-"*10)
+print('123411111111111\r33567')
+print('1234\f33567')
+print('1234\v33567')
+print("\x41\x41")
+print("a\102c")
+print("""Első sor
+Második sor""")
+'''
+x=12
+y=13
+print("x =",x,";","y =",y)
+print(1,2,3,4,5)
+print(1,',',2,',',3,',',4,',',5)
+print(1,2,3,4,5,sep=", ")
+print(x,"*",y,"=",x*y)
+x = +1
+print("x =",x,";","y =",y)
+x +=1                       # x = x + 1
+print("x =",x,";","y =",y)
+x = "Alma"
+print("x =",x,";","y =",y)
+print(x,"*",y,"=",x*y)
+print(x,"*",y,"=",y*x)
