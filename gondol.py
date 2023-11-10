f14e4c68586d063282a4a90b9e22cfba369de747
Gerendás Zoltán <gerendas.zoltan@wmtech.hu>
2023-11-10 13:34:39 +0100

    Gondol2

--- gondol.py
+++ gondol.py
@@ -6,14 +6,26 @@ for i in range(10):
 for i in range(100):
     print(random.randint(1,100),end=", ")
 '''
+# random.seed(0)
 
 gondolt = random.randint(1,100)
+talalt = False
+tippSzam = 0
 
-egesz = 0 
-while egesz < 1 or egesz > 100 :   
-    egesz = int(input("Kérek egy 1 és 100 közé eső számit: "))
+while not talalt:   
+    egesz = 0 
+    while egesz < 1 or egesz > 100 :   
+        egesz = int(input("Kérek egy 1 és 100 közé eső számit: "))
+    tippSzam += 1
+    if gondolt < egesz:
+        print("Nagy")
+    elif gondolt > egesz:
+        print("Kicsi")
+    else:
+        print(tippSzam," Lépésben talált")
+        talalt = True
 
-print(egesz)
+print('-'*20)
 
 
 
